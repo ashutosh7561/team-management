4053222c95b763723d5021c458af23c8d81c27c8
Ashutosh Verma <ashutoshverma7561@gmail.com>
2020-12-08 23:53:02 +0530

    Add module for generating and validating password

--- production/model/validator.py
+++ production/model/validator.py
@@ -4,11 +4,13 @@ from os.path import dirname, abspath
 d = dirname(dirname(abspath(__file__)))
 sys.path.append(d)
 
+from model.hashalgo import hash_password, verify_password
+
 
 def get_password_hash_from_database(username):
     # connection to database will come here
     # it will pass username and will recieve password_hash
-    return username + "hash"
+    return hash_password(username)
 
 
 def get_password_hash(password):
@@ -17,10 +19,10 @@ def get_password_hash(password):
 
 
 def validate_user(username, password):
-    password_hash = get_password_hash(password)
+    # password_hash = get_password_hash(password)
     orignal_hash = get_password_hash_from_database(username)
-    print(password_hash, orignal_hash)
-    if password_hash == orignal_hash:
+    # print(password_hash, orignal_hash)
+    if verify_password(orignal_hash, password):
         print("correct credentials")
         return True
     else:
