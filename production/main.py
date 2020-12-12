e8f62d9f60cc74e0a99e66ecbf8fb668eb13e662
Ashutosh Verma <ashutoshverma7561@gmail.com>
2020-12-12 19:06:21 +0530

    Setup IPC for MVC architecture

--- production/main.py
+++ production/main.py
@@ -10,59 +10,77 @@ import time
 
 def root():
     print("root process")
-    root_conn_view, view_conn = mp.Pipe(duplex=True)
-    root_conn_controller, controller_conn = mp.Pipe(duplex=True)
-    root_conn_model, model_conn = mp.Pipe(duplex=True)
+    view_queue = mp.Queue()
+    controller_queue = mp.Queue()
+    model_queue = mp.Queue()
+    root_queue = mp.Queue()
 
-    view_process = mp.Process(target=view, args=(view_conn,))
-    controller_process = mp.Process(target=controller, args=(controller_conn,))
-    model_process = mp.Process(target=model, args=(model_conn,))
+    view_process = mp.Process(
+        target=view,
+        args=(
+            view_queue,
+            controller_queue,
+            model_queue,
+            root_queue,
+        ),
+    )
+    controller_process = mp.Process(
+        target=controller,
+        args=(
+            view_queue,
+            controller_queue,
+            model_queue,
+            root_queue,
+        ),
+    )
+    model_process = mp.Process(
+        target=model,
+        args=(
+            view_queue,
+            controller_queue,
+            model_queue,
+            root_queue,
+        ),
+    )
 
-    controller_process.start()
     view_process.start()
+    controller_process.start()
     model_process.start()
 
-    # while root_conn_controller.recv() != 100:
-    #     pass
-    # root_conn_view.send(100)
-
-    t = root_conn_controller.recv()
-    while t < 100:
-        t = root_conn_controller.recv()
-        root_conn_view.send(t)
-
-    controller_process.join()
     view_process.join()
+    controller_process.join()
     model_process.join()
 
     print("doing cleanpup")
-    time.sleep(5)
 
 
-def view(handle):
-    valv = handle.recv()
-    while valv != 100:
-        print("simulating event loop", "percentage completion", "-" * valv)
-        # time.sleep(1)
-        valv = handle.recv()
-    for i in range(10):
-        print("hey wait for me")
+def view(view_queue, controller_queue, model_queue, root_queue):
+    time.sleep(3)
+    print("view process")
+    while not (view_queue.empty()):
+        print("view queue :", view_queue.get())
         time.sleep(1)
 
 
-def controller(handle):
+def controller(view_queue, controller_queue, model_queue, root_queue):
+    time.sleep(1)
     print("controller process")
-    print("doing preloading")
-    for i in range(11):
-        handle.send(i * 10)
-        # print(i * 10)
+    for i in range(12, 21):
+        if controller_queue.empty():
+            print("waiting for model to put data")
+        j = controller_queue.get()
+        print(f"putting {i * j} in view_queue")
+        view_queue.put(i * j)
         time.sleep(1)
-    # handle.send("i have loaded")
-    print("controller ready")
 
 
-def model(handle):
+def model(view_queue, controller_queue, model_queue, root_queue):
+    time.sleep(2)
     print("model process")
+    for i in range(10):
+        controller_queue.put(i)
+        print(f"putting {i} in controller_queue")
+        time.sleep(1)
 
 
 if __name__ == "__main__":
