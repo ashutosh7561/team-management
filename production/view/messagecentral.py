33a816342a64bf63d6e253a0b3099d57fac385a2
Ashutosh Verma <ashutoshverma7561@gmail.com>
2020-12-27 02:25:26 +0530

    Fix resizing bug in chat_box

--- production/view/messagecentral.py
+++ production/view/messagecentral.py
@@ -22,7 +22,10 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtCore import QCoreApplication, QMetaObject, QRect, QSize, Qt
 from PyQt5.QtGui import QFont
 
-from view.messagetexttemplate import MessageTextTemplate
+from view.messagetexttemplate import (
+    MessageTextTemplate,
+    MessageTextRecieveTemplate,
+)
 
 
 class ChatWidgetTemplate(QWidget):
@@ -78,6 +81,7 @@ class ChatBoxTemplate(QWidget):
         uic.loadUi(r"./production/view/chat_box_template.ui", self)
         self.chat_heading.setText("Alex")
         self.send.clicked.connect(self.send_message)
+        self.bt.clicked.connect(self.recieve_message)
         self.fbor = QWidget()
         self.vbox = QVBoxLayout()
         self.fbor.setLayout(self.vbox)
@@ -88,14 +92,18 @@ class ChatBoxTemplate(QWidget):
         km = iter(self.fbor.children())
         next(km)
         for i in km:
-            wid = self.set_message_size(i)
+            if isinstance(i, MessageTextTemplate):
+                wid = self.set_message_size(i)
+                # i.self_evaluate()
+            elif isinstance(i, MessageTextRecieveTemplate):
+                wid = self.set_message_receive_size(i)
         super().resizeEvent(event)
 
     def set_message_size(self, msg_widget):
         horizontal_spacer = msg_widget.children()[0].itemAt(0)
         msg_text = msg_widget.message_text
         msg_widget_width = msg_text.fontMetrics().boundingRect(msg_text.text()).width()
-        left_bound = self.width() * 0.66
+        left_bound = self.width() * 0.5
 
         if msg_widget_width < left_bound:
             msg_text.setWordWrap(False)
@@ -103,6 +111,47 @@ class ChatBoxTemplate(QWidget):
                 200, 20, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum
             )
         else:
+            msg_text.setWordWrap(True)
+            horizontal_spacer.changeSize(
+                200, 20, QSizePolicy.Fixed, QSizePolicy.Minimum
+            )
+            if msg_widget.message_text.width() >= msg_widget_width:
+                msg_text.setWordWrap(False)
+                horizontal_spacer.changeSize(
+                    200, 20, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum
+                )
+
+        if msg_widget.width() > self.width():
+            msg_text.setWordWrap(True)
+            horizontal_spacer.changeSize(
+                200, 20, QSizePolicy.Fixed, QSizePolicy.Minimum
+            )
+        horizontal_spacer.invalidate()
+        msg_text.updateGeometry()
+
+    def set_message_receive_size(self, msg_widget):
+        horizontal_spacer = msg_widget.children()[0].itemAt(1)
+        msg_text = msg_widget.message_text
+        msg_widget_width = msg_text.fontMetrics().boundingRect(msg_text.text()).width()
+        left_bound = self.width() * 0.5
+
+        if msg_widget_width < left_bound:
+            msg_text.setWordWrap(False)
+            horizontal_spacer.changeSize(
+                200, 20, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum
+            )
+        else:
+            msg_text.setWordWrap(True)
+            horizontal_spacer.changeSize(
+                200, 20, QSizePolicy.Fixed, QSizePolicy.Minimum
+            )
+            if msg_widget.message_text.width() >= msg_widget_width:
+                msg_text.setWordWrap(False)
+                horizontal_spacer.changeSize(
+                    200, 20, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum
+                )
+
+        if msg_widget.width() > self.width():
             msg_text.setWordWrap(True)
             horizontal_spacer.changeSize(
                 200, 20, QSizePolicy.Fixed, QSizePolicy.Minimum
@@ -118,11 +167,23 @@ class ChatBoxTemplate(QWidget):
         self.set_message_size(m)
 
         self.vbox.addWidget(m)
+        self.scroll_to_last()
+
+    def scroll_to_last(self):
 
         scroll_bar = self.chat_message_list.verticalScrollBar()
 
         scroll_bar.setMaximum(self.fbor.height())
         scroll_bar.setValue(self.fbor.height())
+
+    def recieve_message(self):
+        msg_text = self.input_field.text()
+        m = MessageTextRecieveTemplate()
+        m.message_text.setText(msg_text)
+
+        self.set_message_receive_size(m)
+        self.vbox.addWidget(m)
+        self.scroll_to_last()
 
 
 class Main(QWidget):
--- production/view/messagetexttemplate.py
+++ production/view/messagetexttemplate.py
@@ -28,6 +28,36 @@ class MessageTextTemplate(QWidget):
         super().__init__(parent)
         uic.loadUi(r"./production/view/message_text_template.ui", self)
 
+    def self_evaluate(self):
+        horizontal_spacer = self.children()[0].itemAt(0)
+        msg_text = self.message_text
+        msg_widget_width = msg_text.fontMetrics().boundingRect(msg_text.text()).width()
+        left_bound = self.parent().width() * 0.5
+        print(f"{msg_widget_width} < {left_bound}")
+        print(self.message_text.width())
+        print("window width:", self.parent(), self.parent().width())
+
+        if msg_widget_width < left_bound:
+            print("one liner")
+            msg_text.setWordWrap(False)
+            horizontal_spacer.changeSize(
+                200, 20, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum
+            )
+        else:
+            print("multiline")
+            msg_text.setWordWrap(True)
+            horizontal_spacer.changeSize(
+                200, 20, QSizePolicy.Fixed, QSizePolicy.Minimum
+            )
+        horizontal_spacer.invalidate()
+        msg_text.updateGeometry()
+
+
+class MessageTextRecieveTemplate(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        uic.loadUi(r"./production/view/message_text_recieve_template.ui", self)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
